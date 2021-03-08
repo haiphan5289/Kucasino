48258cb3d2d653ab01c7ee594cb60bfcedb81c25
Phan Gia Thai <thaigp@paxcreation.com>
2021-03-08 09:48:21 +0700

    initati

---
+++ KucasinoApp/KucasinoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KucasinoApp
+//
+//  Created by paxcreation on 3/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
